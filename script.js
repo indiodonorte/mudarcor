833b3426b25064fddb972397e05bc6363f7ef673
Lucas da Silva Figueiredo <ucadonorte@gmail.com>
2024-02-27 22:41:29 -0300

    Delete script.js
    ajuste

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var button = document.getElementById("changeColorBtn");
-    button.addEventListener("click", function() {
-        var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-        document.body.style.backgroundColor = randomColor;
-    });
-});
